0fa96ad8481a09ed0b4b72109144559a04bbd167
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2025-12-10 12:26:04 +0800

    feat: Layout Guide and Custom Errors

--- Raffle.sol
+++ Raffle.sol
@@ -1,3 +1,26 @@
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 //SODX-License-Identifier: MIT-License
 pragma solidity 0.8.19;
 
@@ -7,14 +30,30 @@ pragma solidity 0.8.19;
  * @notice Implementing a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
+
+
+
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public{
+    function enterRaffle() public payable {
+        /* Old versions; conditional version is also an old version
+        require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        */
+        /* Latest version but cannot use due to solidity version
+        However, the conditional version saves more gas
+        require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle());
+        */
+        if (msg.value < i_entranceFee) {
+            Raffle__SendMoreToEnterRaffle();
+        } 
     }
 
     function pickWinner() public{
